89b3d64b1e77229e691352a0cf2a44f06becf0fa
k1x <3296260+k1x0r@users.noreply.github.com>
2013-04-08 15:45:21 +0300

    Modified layout of userProfile and fullTweet

--- twitterList/gen/com/k1x/android/twitterlist/R.java
+++ twitterList/gen/com/k1x/android/twitterlist/R.java
@@ -150,12 +150,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int userview_follow=0x7f020014;
     }
     public static final class id {
-        public static final int actionButtonsLayout=0x7f040024;
+        public static final int actionButtonsLayout=0x7f040021;
         public static final int activity_content=0x7f04000a;
         public static final int animation_layout=0x7f040015;
         public static final int animation_layout_content=0x7f040008;
         public static final int appinfo=0x7f040006;
-        public static final int block_button=0x7f040026;
+        public static final int block_button=0x7f040023;
         public static final int button_login=0x7f040028;
         public static final int button_playstore=0x7f040007;
         public static final int cell_favorited=0x7f040031;
@@ -165,12 +165,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int cell_tweet=0x7f04002f;
         public static final int cell_tweet_icon=0x7f04002e;
         public static final int error_not_logged_in=0x7f040009;
-        public static final int folow_button=0x7f040025;
+        public static final int folow_button=0x7f040022;
         public static final int fullscreen=0x7f040003;
         public static final int imageView1=0x7f040005;
         public static final int left=0x7f040000;
-        public static final int linearLayout1=0x7f04001d;
-        public static final int linearLayout2=0x7f040020;
+        public static final int linearLayout1=0x7f040024;
+        public static final int linearLayout2=0x7f04001d;
         public static final int margin=0x7f040002;
         public static final int menu_reload_form=0x7f04003e;
         public static final int menu_search=0x7f04003a;
@@ -201,16 +201,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int tweetText=0x7f040016;
         public static final int tweet_button=0x7f04002a;
         public static final int tweet_list_button=0x7f040027;
-        public static final int userProfile_createdAt=0x7f04001c;
-        public static final int userProfile_description=0x7f04001b;
-        public static final int userProfile_folowersBtn=0x7f040022;
-        public static final int userProfile_folowingsBtn=0x7f040021;
+        public static final int userProfile_createdAt=0x7f04001b;
+        public static final int userProfile_description=0x7f04001c;
+        public static final int userProfile_folowersBtn=0x7f04001f;
+        public static final int userProfile_folowingsBtn=0x7f04001e;
         public static final int userProfile_screenName=0x7f040019;
-        public static final int userProfile_tweetsBtn=0x7f040023;
+        public static final int userProfile_tweetsBtn=0x7f040020;
         public static final int userProfile_userAvatar=0x7f040018;
         public static final int userProfile_userName=0x7f04001a;
-        public static final int userinfo_blocked=0x7f04001f;
-        public static final int userinfo_folowing=0x7f04001e;
+        public static final int userinfo_blocked=0x7f040026;
+        public static final int userinfo_folowing=0x7f040025;
         public static final int userinfo_fullname=0x7f040035;
         public static final int userinfo_screenname=0x7f040036;
         public static final int userinfo_user_icon=0x7f040034;
